a54c6876f26eeff00c110ba5e8ac9348e4cb0b7c
ZakWrench <Zak.Wrench@gmail.com>
2023-06-27 15:23:47 +0100

    added 404 handler to home page

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -6,9 +6,26 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If it doesn't,
+	// the http.NotFound() function to send a 404 response to the client.
+	// Importantly, we then return from the handler. If we don't return, the handle
+	// would keep executing and also write the "hello" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	// Define a home handler function which write a byte slice containing
 	//"Hello" as the response body.
 	w.Write([]byte("Hello from Snippetbox.zak.net"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
@@ -16,6 +33,8 @@ func main() {
 	// Register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	//Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
@@ -26,5 +45,4 @@ func main() {
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-
 }
