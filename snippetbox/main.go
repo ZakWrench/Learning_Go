4e849fafdb8ab1138d25df704339dba77d51f31b
ZakWrench <Zak.Wrench@gmail.com>
2023-06-27 15:02:44 +0100

    setting a starter webserver

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	// Define a home handler function which write a byte slice containing
+	//"Hello" as the response body.
+	w.Write([]byte("Hello from Snippetbox.zak.net"))
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	//Use the http.NewServerMux() function to intialize a new servemux, then
+	// Register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	//Use the http.ListenAndServe() function to start a new web server. We pass in
+	// two parameters: the TCP network address to listen on (in this case ":4000")
+	// and the servemux we created. If http.ListenAndServe() returns an error
+	// we use the log.Fatal() function to log the error message and exit.
+	// Note that any error returned by http.ListenAndServe() is always non-nil.
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
 }
