1648de393a32ef0ef0c289181e2794387eb4003e
ZakWrench <Zak.Wrench@gmail.com>
2023-07-25 16:03:18 +0100

    added moar stuffs to main.go

--- fundamentals_2/main.go
+++ fundamentals_2/main.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("I'm tired of Hello World examples")
+	var a int
+	var b, c int = 2, 3
+	var d bool = true
+	var e string = "goodbye"
+	var f float32 = 1.68
+	var g complex128 = cmplx.Sqrt(-12 + 14i)
+
+	var arr [20]int
+	var arr_multi [10][5]int // multi-dimensional array
+
+	var sli_ce []int
+
 }
